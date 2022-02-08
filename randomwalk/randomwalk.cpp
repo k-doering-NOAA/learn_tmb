d8a62b601dc2b8dda1271385e1e88f4d59ea6db3
Kathryn Doering <kathryn.doering@noaa.gov>
2022-02-08 10:38:32 -0500

    Changes to (hopefully) get closer to a correct model?
    Still not sure about the likelihood equations.

--- randomwalk/randomwalk.cpp
+++ randomwalk/randomwalk.cpp
@@ -4,28 +4,33 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
   // Data
-  DATA_VECTOR(y);
+  DATA_INTEGER(t); // Length of the y time series (way to calc within tmb?)
+  DATA_INTEGER(n); // Number of time series observed (way to count within tmb?)
+  DATA_MATRIX(y); // Has n rows and t columns
 
   // Parameters
-  PARAMETER_VECTOR(mu_exp); // this may not be a parameter? not sure.
   PARAMETER(mu);
-  //PARAMETER(mu_init);
-  PARAMETER(sigma_proc);
+  PARAMETER(sigma_proc); //to do: may want to put these on log scale for better likelihood surface?
   PARAMETER(sigma_obs);
-  // calcs
-  int timeSteps=y.size();
-  mu_exp(0) = 0;
-  for (int i = 0; i < (timeSteps - 1); i++) {
+
+  // calcs, expected values for time series
+  vector<Type> mu_exp(t);
+  for (int i = 0; i < (t - 1); i++) {
+  mu_exp[0] = Type(0.0);
     mu_exp(i+1) = mu + mu_exp(i);
   }
 
   // objective function
   Type nll = 0.0; // Declare the "objective function" (neg. log. likelihood)
-  // Contribution for the mu_exp values
-  //nll += -dnorm(mu_exp(0), 0, sigma_proc, true); //need something like this?
-  nll += -sum(dnorm(mu_exp, mu, sigma_proc, true)); // Use R-style call to normal density
+  // Contribution for first 2 equations???
+  nll += -dnorm(mu_exp[0], Type(0.0), sigma_proc, true); //need something like this?
+  for(int i = 0; i < (t-1); i++) {
+    nll += -dnorm(mu_exp(i+1), mu + mu_exp(i), sigma_proc, true);
+  }
   // contribution for the y values
-  nll += -sum(dnorm(y, mu_exp, sigma_obs, true)); // Use R-style call to normal density
+  for(int i = 0; i < n; i++) {
+    nll += -sum(dnorm(y(i), mu_exp, sigma_obs, true));
+  }
 
   return nll;
 }
