6a913755a7038ceb423b8beae03f0f691eceb311
Kathryn Doering <kathryn.doering@noaa.gov>
2022-02-11 11:26:28 -0500

    corrections based on discussion sections

--- randomwalk/randomwalk.cpp
+++ randomwalk/randomwalk.cpp
@@ -8,22 +8,21 @@ Type objective_function<Type>::operator() ()
   DATA_INTEGER(n); // Number of time series observed (way to count within tmb?)
   DATA_MATRIX(y); // Has n rows and t columns
 
+  //TODO: add mu_exp as a parameter
+
   // Parameters
   PARAMETER(mu);
   PARAMETER(log_sigma_proc);
   PARAMETER(log_sigma_obs);
-
-  // calcs, expected values for time series
-  vector<Type> mu_exp(t);
-  for (int i = 0; i < (t - 1); i++) {
-  mu_exp[0] = Type(0.0);
-    mu_exp(i+1) = mu + mu_exp(i);
-  }
+  PARAMETER_VECTOR(mu_exp);
 
   // objective function
+  Type t_test = y.cols();
+  Type n_test = y.rows();
+
   Type nll = 0.0; // Declare the "objective function" (neg. log. likelihood)
-  // Contribution for first 2 equations???
-  nll += -dnorm(mu_exp[0], Type(0.0), exp(log_sigma_proc), true); //need something like this?
+  // Contribution for first equations
+  nll += -dnorm(mu_exp(0), Type(0.0), exp(log_sigma_proc), true);
   for(int i = 0; i < (t-1); i++) {
     nll += -dnorm(mu_exp(i+1), mu + mu_exp(i), exp(log_sigma_proc), true);
   }
@@ -31,6 +30,9 @@ Type objective_function<Type>::operator() ()
   for(int i = 0; i < n; i++) {
     nll += -sum(dnorm(y(i), mu_exp, exp(log_sigma_obs), true));
   }
+
+  REPORT(t_test);
+  REPORT(n_test);
 
   return nll;
 }
